405c152419f5316b79370e611b02b243ed59f215
Levi Bohn <levibohn@gmail.com>
2023-03-09 20:55:34 -0600

    successfully rendered six names to the DOM.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CharacterList from "./components/Character";
 
-const App = () => {
+function App() {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        console.log(res);
+        setData(res.data);
+      })
+      .catch(err => console.error(err))
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+        { data && <CharacterList name={data} /> }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React from 'react';
+
+const CharacterList = (props) => {
+    return (
+        <div>
+            <p>{props.name[0].name}</p>
+            <p>{props.name[1].name}</p>
+            <p>{props.name[2].name}</p>
+            <p>{props.name[3].name}</p>
+            <p>{props.name[4].name}</p>
+            <p>{props.name[5].name}</p>
+        </div>
+    )
+}
+
+const rootElement = document.getElementById("root");
+
+// ReactDOM.render(
+//     <CharacterList />,
+//     rootElement
+// );
+
+export default CharacterList;
